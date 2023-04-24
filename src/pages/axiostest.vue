8e90d442e22b64a31cb39e44c72a0754ceabc1e9
Asher Lien <birdym.tw@gmail.com>
2023-04-24 11:00:34 +0800

    引入axiost成功

---
+++ src/pages/axiostest.vue
@@ -0,0 +1,12 @@
+<script setup>
+import axios from 'axios';
+onMounted(() => {
+  axios.get('https://vue-lessons-api.vercel.app/courses/list').then(res => {
+    console.log(res);
+  });
+});
+</script>
+<template>
+  <h1>1adjslasld;saj</h1>
+</template>
+<style></style>
