547ba2fec7fb01480fee6c94ec2fde31af49e782
Asher Lien <birdym.tw@gmail.com>
2023-05-01 11:38:13 +0800

    完成後台初步UI

---
+++ src/components/ProductManage/ProductSelectPDM.vue
@@ -0,0 +1,38 @@
+<template>
+  <!-- 商品類別選單： -->
+  <el-select v-model="value" class="m-2" placeholder="商品分類">
+    <el-option
+      v-for="item in options"
+      :key="item.value"
+      :label="item.label"
+      :value="item.value"
+    />
+  </el-select>
+</template>
+
+<script setup>
+const value = ref('');
+
+const options = [
+  {
+    value: 'Option1',
+    label: 'Option1',
+  },
+  {
+    value: 'Option2',
+    label: 'Option2',
+  },
+  {
+    value: 'Option3',
+    label: 'Option3',
+  },
+  {
+    value: 'Option4',
+    label: 'Option4',
+  },
+  {
+    value: 'Option5',
+    label: 'Option5',
+  },
+];
+</script>
