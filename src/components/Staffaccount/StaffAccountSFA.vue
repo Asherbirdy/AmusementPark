393761316ec981503d5f8f1d4aa5869edbb72410
rusheep <yo0983987618@gmail.com>
2023-05-03 16:30:56 +0800

    串資完成

--- src/components/Staffaccount/StaffAccountSFA.vue
+++ src/components/Staffaccount/StaffAccountSFA.vue
@@ -1,12 +1,12 @@
 <template>
-  <el-table :data="filterTableData" style="width: 100%">
+  <el-table :data="tableData" style="width: 100%">
     <el-table-column label="員工編號" prop="id" />
-    <el-table-column label="員工帳號" prop="Account" />
-    <el-table-column label="員工密碼" prop="PWD" />
-    <el-table-column label="員工權限" prop="permissions" />
+    <el-table-column label="員工帳號" prop="account" />
+    <el-table-column label="權限" prop="permissions" />
     <el-table-column align="right">
-      <!-- <el-input v-model="search" size="small" placeholder="Type to search" /> -->
-
+      <template #header>
+        <el-input v-model="search" size="small" placeholder="Type to search" />
+      </template>
       <template #default="scope">
         <el-button size="small" @click="handleEdit(scope.$index, scope.row)"
           >Edit</el-button
@@ -23,33 +23,51 @@
 </template>
 
 <script setup>
-import { computed, ref } from 'vue';
 import axios from 'axios';
+import { computed, ref } from 'vue';
 
-let tableData = reactive([]);
+let tableData = ref([]);
 
-axios
-  .post('/api/PDO/Select.php', {})
-  .then(res => {
-    const data = res.data.reduce((obj, item) => {
-      obj[item.id] = item;
-      return obj;
-    }, {});
-    Object.assign(tableData, data);
-  })
-  .catch(err => {
-    console.log(err);
-    alert('伺服器問題');
+
+
+onMounted(() => {
+  axios.get('/api/PDO/staffAccount/staffAccountSelect.php').then(res => {
+    // API 抓取到的資料：
+    const data = res.data;
+    console.log(data);
+
+
+    // 將資料轉成 element 可以讀的參數，參考 public/json/facility_status.json
+    const staffData = data.map(staff => ({
+      id: staff.id,
+      account: staff.Account,
+      permissions: staff.permissions,
+      // date:
+      //   facility.startDate && facility.endDate
+      //     ? `${facility.startDate} 至 ${facility.endDate}`
+      //     : '',
+    }));
+    //
+
+    tableData.value = staffData;
+    console.log(tableData);
+
+
   });
+});
 
-const search = ref('');
-const filterTableData = computed(() =>
-  tableData.filter(
-    data =>
-      !search.value ||
-      data.name.toLowerCase().includes(search.value.toLowerCase())
-  )
-);
-
-console.log(filterTableData);
+// const search = ref('');
+// const filterTableData = computed(() =>
+//   tableData.filter(
+//     data =>
+//       !search.value ||
+//       data.name.toLowerCase().includes(search.value.toLowerCase())
+//   )
+// );
+// const handleEdit = (index: number, row: User) => {
+//   console.log(index, row);
+// };
+// const handleDelete = (index: number, row: User) => {
+//   console.log(index, row);
+// };
 </script>
