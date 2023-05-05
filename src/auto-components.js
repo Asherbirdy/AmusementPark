c2c5c2282139ede246538592d2570795557beaf0
zhuanqiu <zhuanqiu84@gmail.com>
2023-05-05 21:01:23 +0800

    新增文字

--- src/auto-components.js
+++ src/auto-components.js
@@ -12,22 +12,17 @@ declare module '@vue/runtime-core' {
     ActivityList: typeof import('./components/Activity/ActivityList.vue')['default']
     AdminNav: typeof import('./components/-Global/AdminNav.vue')['default']
     Axiostest: typeof import('./components/-Global/Axiostest.vue')['default']
-<<<<<<< HEAD
-=======
-    ElButton: typeof import('element-plus/es')['ElButton']
-    ElForm: typeof import('element-plus/es')['ElForm']
-    ElFormItem: typeof import('element-plus/es')['ElFormItem']
-    ElInput: typeof import('element-plus/es')['ElInput']
-    ElText: typeof import('element-plus/es')['ElText']
+    Btn: typeof import('./components/-UI Element/btn/Btn.vue')['default']
     H: typeof import('./components/-Global/H.vue')['default']
     Header: typeof import('./components/-Global/Header.vue')['default']
->>>>>>> origin/master
     IconCommunity: typeof import('./components/-UI Element/icons/IconCommunity.vue')['default']
     IconDocumentation: typeof import('./components/-UI Element/icons/IconDocumentation.vue')['default']
     IconEcosystem: typeof import('./components/-UI Element/icons/IconEcosystem.vue')['default']
     IconSupport: typeof import('./components/-UI Element/icons/IconSupport.vue')['default']
     IconTooling: typeof import('./components/-UI Element/icons/IconTooling.vue')['default']
     IconUserProfile: typeof import('./components/-UI Element/icons/IconUserProfile.vue')['default']
+    Input: typeof import('./components/-UI Element/form/Input.vue')['default']
+    ModalS: typeof import('./components/-UI Element/modalframe/ModalS.vue')['default']
     ParkStatusTablePS: typeof import('./components/ParkStatus/ParkStatusTablePS.vue')['default']
     ProductListPDS: typeof import('./components/Productstatus/ProductListPDS.vue')['default']
     ProductSelectPDM: typeof import('./components/ProductManage/ProductSelectPDM.vue')['default']
@@ -39,5 +34,6 @@ declare module '@vue/runtime-core' {
     SvgIcon: typeof import('./components/SvgIcon.vue')['default']
     TagsTable: typeof import('./components/ProductManage/TagsTable.vue')['default']
     TicketListPDS: typeof import('./components/Productstatus/TicketListPDS.vue')['default']
+    TitleBig: typeof import('./components/-UI Element/title/TitleBig.vue')['default']
   }
 }
