ad0d650c6f95fff4a96a9628ea9327dc96a67b52
shuyu36 <sharon87306@gmail.com>
2023-05-07 13:52:06 +0800

    更正內容

--- src/pages/admin/cartfill.vue
+++ src/pages/admin/cartfill.vue
@@ -1,5 +1,4 @@
 <template lang="">
-
   <article>
     <div class="CartStep">
       <SvgMonsterFrame />
@@ -13,128 +12,130 @@
     <div class="user">
       <h2>訂購人資料</h2>
       <form>
-        <label for="name">姓名：</label>
-        <br />
-        <input
-          type="text"
-          id="name"
-          v-model="Name"
-          placeholder="請輸入您的姓名"
-        />
-        <br />
-
-        <label for="phone-number">手機號碼：</label>
-        <br />
-        <input
-          type="tel"
-          id="phone-number"
-          v-model="phoneNumber"
-          placeholder="請輸入您的手機號碼"
-        />
-        <br />
-
-        <label for="email">電子郵件：</label>
-        <br />
-        <input
-          type="text"
-          id="email"
-          v-model="Email"
-          placeholder="請輸入您的電子郵件"
-        />
-        <br />
-
-        <label for="address">寄送地址：</label>
-        <br />
-        <input
-          type="text"
-          id="address"
-          v-model="Address"
-          placeholder="請輸入您的地址"
-        />
-        <br />
-
+        <div
+          v-for="(item, index) in orderInfo"
+          :key="item.id"
+          class="user-input-row"
+        >
+          <label>{{ item.title }}</label>
+          <input
+            :type="item.type"
+            :id="item.id"
+            v-model="item.value"
+            :placeholder="item.placeholder"
+          />
+        </div>
         <label for="comment">訂單備註：</label>
-        <br />
         <textarea
           id="comment"
-          v-model="Comment"
+          v-model="comment"
           cols="20"
           rows="7"
           placeholder="請填寫您的訂單備註*"
           resize="none"
-        ></textarea>
-        <br />
+        >
+        </textarea>
       </form>
     </div>
 
     <div class="pay">
-      <h2>付款方式：</h2>
+      <h2>付款方式</h2>
       <form>
         <label for="pay-method">付款方式：</label>
-        <br />
-        <select id="pay-method" v-model="PayMethod">
-          <option value="credit-card">信用卡(支援國內外Visa,Master)</option>
+        <select id="pay-method" v-model="payMethod">
+          <option
+            v-for="(option, index) in payOption"
+            :value="option.value"
+            :key="index"
+          >
+            {{ option.label }}
+          </option>
+        </select>
+
+        
+      </form>
+    </div>
+
+    <!-- <option value="credit-card">信用卡(支援國內外Visa,Master)</option>
           <option value="line-pay">Line Pay</option>
           <option value="apple-pay">Apple Pay</option>
         </select>
         <br />
-
         <label for="address" class="pay-label">付款資料：</label>
         <input
-          type="text"
+          type="tet"
           id="card-number"
           v-model="CardNumber"
           placeholder="卡號"
         />
-
         <input type="text" id="name" v-model="Name" placeholder="持卡人姓名" />
-
         <input
           type="month"
           id="date"
           v-model="Date"
           placeholder="有效期(MM/YY)"
         />
-
         <input type="password" id="code" v-model="Code" placeholder="安全碼" />
-
         <button type="submit" id="Submit">提交訂單</button>
-      </form>
-    </div>
+      </form> -->
+    <!-- </div> -->
   </main>
 </template>
-<script>
-export default {
-  data() {
-    return {
-      Name: '',
-      phoneNumber: '',
-      Email: '',
-      Address: '',
-      Comment: '',
-      PayMethod: 'credit-card',
-      CardNumber: '',
-      Date: '',
-      Code: '',
-      PayMethod: 'credit-card',
-    };
+
+<script setup>
+const name = '';
+const phoneNumber = '';
+const email = '';
+const address = '';
+const comment = '';
+
+const orderInfo = ref([
+  {
+    title: '姓名：',
+    type: 'text',
+    id: 'name',
+    value: name,
+    placeholder: '請輸入您的姓名',
   },
-};
+  {
+    title: '手機號碼：',
+    type: 'tel',
+    id: 'phone-number',
+    value: phoneNumber,
+    placeholder: '請輸入您的手機號碼',
+  },
+  {
+    title: '電子郵件',
+    type: 'text',
+    id: 'email',
+    value: email,
+    placeholder: '請輸入您的電子郵件',
+  },
+  {
+    title: '寄送地址：',
+    type: 'text',
+    id: 'address',
+    value: address,
+    placeholder: '請輸入您的地址',
+  },
+]);
+const Comment = ref([comment]);
+
+let payMethod = '';
 </script>
 <style lang="scss" scoped>
-
-article{
+article {
   display: flex;
   justify-content: space-evenly;
   margin-top: 200px;
 }
-.CartStep{
+.CartStep {
   display: flex;
   align-items: center;
 }
 
-.test{
-  border-bottom:2px dashed #D1825B;
+.test {
+  border-bottom: 2px dashed #d1825b;
   width: 100px;
 }
 
@@ -149,7 +150,6 @@ main {
   background-color: #f9f3e4;
   margin: 15px;
   margin-top: 100px;
-  background-image: url(../../../src/components/-UI\ Element/svg/Vector.svg);
 }
 
 .user {
@@ -158,6 +158,27 @@ main {
   background-color: #f9f3e4;
   margin: 15px;
   margin-top: 100px;
+  .user-input-row {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    margin-bottom: 20px;
+    font-size: 16px;
+    font-weight: bold;
+    color: #90420a;
+    label {
+      margin-bottom: 10px;
+    }
+    // input {
+    //   width: -webkit-fill-available;
+    // }
+  }
+  #comment {
+    resize: none;
+    padding: 10px;
+    border: 2px solid #000000;
+    border-radius: 10px;
+  }
 }
 
 h2 {
@@ -172,6 +193,7 @@ label {
   font-size: 16px;
   font-weight: bold;
   color: #90420a;
+  margin-bottom: 10px;
 }
 
 form {
@@ -188,14 +210,6 @@ input {
 }
 
 select {
-  padding: 10px;
-  border: 2px solid #000000;
-  border-radius: 10px;
-}
-
-textarea {
-  resize: none;
-
   padding: 10px;
   border: 2px solid #000000;
   border-radius: 10px;
@@ -218,5 +232,4 @@ textarea {
 .pay input {
   margin-bottom: 23px;
 }
-
 </style>
