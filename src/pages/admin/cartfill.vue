8d0d54f1f1fbd11e3c1fde6d3f770d830a85daab
shuyu36 <sharon87306@gmail.com>
2023-05-08 09:42:20 +0800

    已修改表單寫法

--- src/pages/admin/cartfill.vue
+++ src/pages/admin/cartfill.vue
@@ -20,7 +20,7 @@
           <label>{{ item.title }}</label>
           <input
             :type="item.type"
-            :id="item.id"
+            :id="index"
             v-model="item.value"
             :placeholder="item.placeholder"
           />
@@ -33,8 +33,7 @@
           rows="7"
           placeholder="請填寫您的訂單備註*"
           resize="none"
-        >
-        </textarea>
+        ></textarea>
       </form>
     </div>
 
@@ -43,6 +42,7 @@
       <form>
         <label for="pay-method">付款方式：</label>
         <select id="pay-method" v-model="payMethod">
+          <option value="" disabled selected>請選擇付款方式</option>
           <option
             v-for="(option, index) in payOption"
             :value="option.value"
@@ -52,33 +52,23 @@
           </option>
         </select>
 
-        
+        <label for="payData">付款資料：</label>
+        <div
+          v-for="(item, index) in payInput"
+          :key="index"
+          class="user-input-row"
+        >
+          <!-- <label for="item.id">{{ item.label }}</label> -->
+          <input
+            :type="item.type"
+            :id="item.id"
+            v-model="item.value"
+            :placeholder="item.placeholder"
+          />
+        </div>
+        <button type="submit" id="Submit">提交訂單</button>
       </form>
     </div>
-
-    <!-- <option value="credit-card">信用卡(支援國內外Visa,Master)</option>
-          <option value="line-pay">Line Pay</option>
-          <option value="apple-pay">Apple Pay</option>
-        </select>
-        <br />
-        <label for="address" class="pay-label">付款資料：</label>
-        <input
-          type="tet"
-          id="card-number"
-          v-model="CardNumber"
-          placeholder="卡號"
-        />
-        <input type="text" id="name" v-model="Name" placeholder="持卡人姓名" />
-        <input
-          type="month"
-          id="date"
-          v-model="Date"
-          placeholder="有效期(MM/YY)"
-        />
-        <input type="password" id="code" v-model="Code" placeholder="安全碼" />
-        <button type="submit" id="Submit">提交訂單</button>
-      </form> -->
-    <!-- </div> -->
   </main>
 </template>
 
@@ -87,7 +77,6 @@ const name = '';
 const phoneNumber = '';
 const email = '';
 const address = '';
-const comment = '';
 
 const orderInfo = ref([
   {
@@ -105,7 +94,7 @@ const orderInfo = ref([
     placeholder: '請輸入您的手機號碼',
   },
   {
-    title: '電子郵件',
+    title: '電子郵件：',
     type: 'text',
     id: 'email',
     value: email,
@@ -119,19 +108,69 @@ const orderInfo = ref([
     placeholder: '請輸入您的地址',
   },
 ]);
-const Comment = ref([comment]);
 
-let payMethod = '';
+const commentLabel = ref([]);
+
+// let payMethod = '';
+let cardNumber = '';
+let cardName = '';
+let date = '';
+let code = '';
+
+const payOption = [
+  
+  { label: '信用卡(支援國內外Visa,Master)', value: 'credit-card'  },
+  { label: 'Line Pay', value: 'line-pay' },
+  { label: 'Apple Pay', value: 'apple-pay' },
+];
+const payInput = [
+  {
+    label: '卡號',
+    type: 'text',
+    id: 'card-number',
+    value: cardNumber,
+    placeholder: '卡號',
+  },
+  {
+    label: '持卡人姓名',
+    type: 'text',
+    id: 'name',
+    value: cardName,
+    placeholder: '持卡人姓名',
+  },
+  {
+    label: '有效期(MM/YY)',
+    type: 'text',
+    id: 'date',
+    value: date,
+    placeholder: '有效期(MM/YY)',
+  },
+  {
+    label: '安全碼',
+    type: 'password',
+    id: 'code',
+    value: code,
+    placeholder: '安全碼',
+  },
+];
 </script>
+
 <style lang="scss" scoped>
+h2 {
+  color: #f9f3e4;
+  background-color: #d1825b;
+  text-align: center;
+  line-height: 70px;
+  height: 70px;
+}
 article {
   display: flex;
   justify-content: space-evenly;
   margin-top: 200px;
-}
-.CartStep {
-  display: flex;
-  align-items: center;
+  .CartStep {
+    display: flex;
+    align-items: center;
+  }
 }
 
 .test {
@@ -150,6 +189,24 @@ main {
   background-color: #f9f3e4;
   margin: 15px;
   margin-top: 100px;
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
+    padding: 40px 50px;
+    #pay-method {
+      margin-bottom: 30px;
+    }
+    .user-input-row {
+      display: flex;
+      flex-direction: column;
+      // margin-bottom: 20px;
+      color: #90420a;
+    }
+    input {
+      margin-bottom: 23px;
+    }
+  }
 }
 
 .user {
@@ -158,35 +215,33 @@ main {
   background-color: #f9f3e4;
   margin: 15px;
   margin-top: 100px;
-  .user-input-row {
+  form {
     display: flex;
     flex-direction: column;
-    width: 100%;
-    margin-bottom: 20px;
-    font-size: 16px;
-    font-weight: bold;
-    color: #90420a;
-    label {
-      margin-bottom: 10px;
+    align-items: stretch;
+    padding: 40px 50px;
+    .user-input-row {
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+      margin-bottom: 20px;
+      font-size: 16px;
+      font-weight: bold;
+      color: #90420a;
+      label {
+        margin-bottom: 10px;
+      }
+      // input {
+      //   width: -webkit-fill-available;
+      // }
     }
-    // input {
-    //   width: -webkit-fill-available;
-    // }
+    #comment {
+      resize: none;
+      padding: 10px;
+      border: 2px solid #000000;
+      border-radius: 10px;
+    }
   }
-  #comment {
-    resize: none;
-    padding: 10px;
-    border: 2px solid #000000;
-    border-radius: 10px;
-  }
-}
-
-h2 {
-  color: #f9f3e4;
-  background-color: #d1825b;
-  text-align: center;
-  line-height: 70px;
-  height: 70px;
 }
 
 label {
@@ -194,13 +249,6 @@ label {
   font-weight: bold;
   color: #90420a;
   margin-bottom: 10px;
-}
-
-form {
-  display: flex;
-  flex-direction: column;
-  align-items: stretch;
-  padding: 40px 50px;
 }
 
 input {
@@ -227,9 +275,5 @@ select {
 }
 .pay-label {
   margin: 30px 0 5px 0;
-}
-
-.pay input {
-  margin-bottom: 23px;
 }
 </style>
