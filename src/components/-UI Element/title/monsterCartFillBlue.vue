18728fce2e6d00cd847fb985eb4aed9d92d1690c
shuyu36 <119433267+shuyu36@users.noreply.github.com>
2023-05-10 11:29:34 +0800

    遊客前台 購物車v2 (#40)
    * 訂購人資料完成
    
    * 付款方式完成
    
    * 付款完成
    
    * 測試
    
    * 更正內容
    
    * 更新
    
    * 已修改表單寫法
    
    * 更新購物車步驟
    
    * bookshowe更新
    
    * 更新
    
    * vue3_遊客前台_購物車v2完成

---
+++ src/components/-UI Element/title/monsterCartFillBlue.vue
@@ -0,0 +1,48 @@
+<template>
+  <h2 :style="{ color: internalTitleColor }"><slot>2</slot></h2>
+  <monsterBlue :style="{ color: internalSvgColor }" class="svg" />
+</template>
+<script>
+export default {
+  props: {
+    titleColor: {
+      type: String,
+      default: '#163767',
+    },
+    svgColor: {
+      type: String,
+      default: '#FFE9DE',
+    },
+  },
+  data() {
+    return {
+      internalTitleColor: this.titleColor,
+      internalSvgColor: this.svgColor,
+    };
+  },
+};
+</script>
+<style lang="scss" scoped>
+.CartStep {
+  width: $workspace;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  h2 {
+    position: absolute;
+    line-height: 155px;
+    font-size: 50px;
+    color: null;
+  }
+  .svg {
+    color: null;
+  }
+
+  slot {
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
