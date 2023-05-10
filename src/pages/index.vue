4d95c5b72a24264bb22550751ee351460c1f02d7
YO-XUAN <61690636+rusheep@users.noreply.github.com>
2023-05-10 13:17:40 +0800

    遊客前台 首頁 (#44)
    * 定義測試資料/頁面正確格式
    
    * axios抓取測試資料
    
    * 抓取資料並轉為格式
    
    * 完成設施狀態燈(綠/黃/紅)
    
    * 依資料顯示開關/設施權限開關
    
    * 完成彈窗UI_30%
    
    * 完成關閉設施_modal介面
    
    * 完成關閉設施_modal介面
    
    * 建檔案
    
    * 切完書本UI
    
    * 安裝yarn
    
    * 首頁切版完成80%
    
    ---------
    
    Co-authored-by: Asher Lien <birdym.tw@gmail.com>

---
+++ src/pages/index.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <VideoBannerIDX />
+  <title-main>aasdas</title-main>
+  <TeaserIDX />
+  <!-- 小怪獸彈窗 -->
+
+  <InformationIDX />
+</template>
+<style scoped lang="scss">
+//scss測試：
+</style>
