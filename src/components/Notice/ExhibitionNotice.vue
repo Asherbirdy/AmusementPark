66a9ecfce4f8f1d2a660ba1c1b75f9d89e9f548c
zhuanqiu <127811780+zhuanqiu@users.noreply.github.com>
2023-05-11 13:09:32 +0800

    遊客前台 領地須知 (#56)
    * 增加title 自大小
    
    * 新增關於我們
    
    * 新增文字
    
    * 新增標題及營業時間表格
    
    * 新增文字
    
    * 新增文字及改成yarn
    
    * 新增圖片
    
    * 新增圖片
    
    * 增加圖片以及主標籤背景換顏色
    
    * merge
    
    * merge
    
    merge
    
    * 新增組件
    
    * 修改egg大小
    
    * 修改標題樣式
    
    * 調整距離、增加ICON
    
    * 修改標題
    
    * add

---
+++ src/components/Notice/ExhibitionNotice.vue
@@ -0,0 +1,65 @@
+<script setup></script>
+<template>
+    <main>
+        <div class="exhi">
+        <TitleBig_yellow class="title" />
+        <h1>展覽特色</h1>
+            <ul>
+                <li>
+                    <img src="@/assets/img/performance4.png" />
+                    <h3>火焰跳躍者</h3>
+                </li>
+                <li>
+                    <img src="@/assets/img/performance5.png" />
+                    <h3>魔幻舞台</h3>
+                </li>
+                <li>
+                    <img src="@/assets/img/performance6.png" />
+                    <h3>夢幻演繹</h3>
+                </li>
+            </ul>
+         </div>
+    </main>
+</template>
+<style lang="scss" scoped>
+main {
+    width: $workspace;
+    .exhi {
+        width: 1200px;
+        margin: 0 auto;
+        position: relative;
+    }
+    .title{
+            margin: 0 350px;
+        }
+    h1{
+        position: absolute;
+        top: 176px;
+        left: 511px;
+        color: #CE5B5B;
+        font-size: 50px;
+    }
+    
+        .exhi ul{
+            margin: 75px 50px;
+            display: flex;
+        }
+        .exhi li{
+            padding-left: 55px;
+        }
+        .exhi li:nth-child(1){
+            transform: rotateY(54deg)
+        }
+        .exhi li:nth-child(3){
+            transform: rotateY(-54deg)
+        }
+        .exhi img{
+            display: block;
+            margin-bottom: 30px;
+        }
+        .exhi h3{
+            text-align: center;
+        }
+}
+
+</style>
