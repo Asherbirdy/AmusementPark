bd40de5a2845aa9001e645c0d9f809d4136860c4
Asherbirdy <birdym.tw@gmail.com>
2023-05-12 06:55:26 +0800

    增加test.vue

--- src/pages/course/test.vue
+++ src/pages/course/test.vue
@@ -1,7 +1,21 @@
 <template>
-  <h1>22222</h1>
+  <div v-for="item in imgData" :key="item.productName">
+    <img :src="item.img" />
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
 
-<style scoped lang="scss"></style>
+const imgData = ref([
+  { productName: 'aaa', img: '/img/bag1.png' },
+  { productName: 'bbb', img: '/img/bag2.png' },
+  { productName: 'ccc', img: 'test/img/bag3.png' },
+]);
+
+const getImgPath = img => {
+  return new URL(img, import.meta.url).href;
+};
+</script>
+
+<style></style>
