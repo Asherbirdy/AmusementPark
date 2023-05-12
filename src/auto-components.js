0bd9620e449f072fffb8c4e7574cdb46e3721b9d
YO-XUAN <61690636+rusheep@users.noreply.github.com>
2023-05-12 11:35:43 +0800

    引入照片工具 (#67)
    * 增加test.vue
    
    * 動態照片引入
    
    * 建立兩版本測試
    
    * 更新測試版本
    
    ---------
    
    Co-authored-by: Asherbirdy <birdym.tw@gmail.com>

--- src/auto-components.js
+++ src/auto-components.js
@@ -15,6 +15,12 @@ declare module '@vue/runtime-core' {
     Axiostest: typeof import('./components/-Global/Axiostest.vue')['default']
     Btn: typeof import('./components/-UI Element/btn/Btn.vue')['default']
     ColorPicker: typeof import('./components/-UI Element/ColorPicker/ColorPicker.vue')['default']
+
+    copy: typeof import('./components/-UI Element/svg/header/icon-login copy.vue')['default']
+    ElButton: typeof import('element-plus/es')['ElButton']
+    ElButtonGroup: typeof import('element-plus/es')['ElButtonGroup']
+    ElCalendar: typeof import('element-plus/es')['ElCalendar']
+
     ElIcon: typeof import('element-plus/es')['ElIcon']
     ExhibitionNotice: typeof import('./components/Notice/ExhibitionNotice.vue')['default']
     Footer: typeof import('./components/-Global/Footer.vue')['default']
@@ -67,7 +73,10 @@ declare module '@vue/runtime-core' {
     RouterView: typeof import('vue-router')['RouterView']
     SearchBarEmailPDS: typeof import('./components/Productstatus/SearchBarEmailPDS.vue')['default']
     SocialIconLGN: typeof import('./components/Login/SocialIconLGN.vue')['default']
+
+
     SocialIconReg: typeof import('./components/Register/SocialIconReg.vue')['default']
+
     StaffAccountSFA: typeof import('./components/Staffaccount/StaffAccountSFA.vue')['default']
     SvgBrush: typeof import('./components/-UI Element/svg/pic/SvgBrush.vue')['default']
     SvgBus: typeof import('./components/-UI Element/svg/pic/SvgBus.vue')['default']
@@ -102,6 +111,7 @@ declare module '@vue/runtime-core' {
     TeaserIDX: typeof import('./components/Index/TeaserIDX.vue')['default']
     TicketBookingTOR: typeof import('./components/TicketOrder/TicketBookingTOR.vue')['default']
     TicketExhibitionTOR: typeof import('./components/TicketOrder/TicketExhibitionTOR.vue')['default']
+    TicketfromTBS: typeof import('./components/TouristBackstage/TicketfromTBS.vue')['default']
     TicketListPDS: typeof import('./components/Productstatus/TicketListPDS.vue')['default']
     TicketPriceTOR: typeof import('./components/TicketOrder/TicketPriceTOR.vue')['default']
     TickOrder: typeof import('./components/TouristBackstage/Element/svg/tickOrder.vue')['default']
