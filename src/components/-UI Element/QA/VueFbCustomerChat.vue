b23aa903684ff394d03fbb3a257e4678ccbb6412
shuyu36 <sharon87306@gmail.com>
2023-05-14 12:40:42 +0800

    更新組件

---
+++ src/components/-UI Element/QA/VueFbCustomerChat.vue
@@ -0,0 +1,11 @@
+<template></template>
+<script>
+import VueFbCustomerChat from 'vue-fb-customer-chat'
+
+export default {
+    components: {
+        VueFbCustomerChat
+    }
+}
+</script>
+<style></style>
