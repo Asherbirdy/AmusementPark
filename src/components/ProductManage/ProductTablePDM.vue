4346af10f3db07c7cfcdb8e0b4d2f7f8fd9af646
Asherbirdy <birdym.tw@gmail.com>
2023-05-15 15:06:48 +0800

    完成 園區後台_商品管理_商品列表UI

--- src/components/ProductManage/ProductTablePDM.vue
+++ src/components/ProductManage/ProductTablePDM.vue
@@ -1,62 +1,119 @@
 <template>
   <el-table :data="tableData" style="width: 100%">
-    <el-table-column fixed prop="date" label="Date" width="150" />
-    <el-table-column prop="name" label="Name" width="120" />
-    <el-table-column prop="state" label="State" width="120" />
-    <el-table-column prop="city" label="City" width="120" />
-    <el-table-column prop="address" label="Address" width="600" />
-    <el-table-column prop="zip" label="Zip" width="120" />
-    <el-table-column fixed="right" label="Operations" width="120">
-      <template #default>
-        <el-button link type="primary" size="small" @click="handleClick"
-          >Detail</el-button
-        >
-        <el-button link type="primary" size="small">Edit</el-button>
-      </template>
-    </el-table-column>
+    <el-table-column fixed prop="category" label="類別" width="150" />
+    <el-table-column prop="productName" label="商品名稱" width="120" />
+    <el-table-column prop="color" label="顏色" width="120" />
+    <el-table-column prop="size" label="尺寸" width="120" />
+    <el-table-column prop="price" label="價格" width="120" />
+    <el-table-column prop="productStatus" label="狀態" width="120" />
+    <el-table-column fixed="right" label="編輯" width="120"> </el-table-column>
   </el-table>
 </template>
 
-<script lang="ts" setup>
+<script setup>
 const handleClick = () => {
   console.log('click');
 };
 
 const tableData = [
   {
-    date: '2016-05-03',
-    name: 'Tom',
-    state: 'California',
-    city: 'Los Angeles',
+    category: '帽子',
+    productName: '超炫帽帽',
+    color: '藍色',
+    size: 'S',
+    price: 2000,
+    productStatus: '上架中',
     address: 'No. 189, Grove St, Los Angeles',
-    zip: 'CA 90036',
     tag: 'Home',
   },
   {
-    date: '2016-05-02',
-    name: 'Tom',
-    state: 'California',
-    city: 'Los Angeles',
+    category: '帽子',
+    productName: '超炫帽帽',
+    color: '紅色',
+    size: 'S',
+    price: 2000,
+    productStatus: '上架中',
     address: 'No. 189, Grove St, Los Angeles',
-    zip: 'CA 90036',
     tag: 'Office',
   },
   {
-    date: '2016-05-04',
-    name: 'Tom',
-    state: 'California',
-    city: 'Los Angeles',
+    category: '帽子',
+    productName: '超炫帽帽',
+    color: '黃色',
+    size: 'S',
+    price: 2000,
+    productStatus: '上架中',
     address: 'No. 189, Grove St, Los Angeles',
-    zip: 'CA 90036',
     tag: 'Home',
   },
   {
-    date: '2016-05-01',
-    name: 'Tom',
-    state: 'California',
-    city: 'Los Angeles',
+    category: '衣服',
+    productName: '超炫衣服',
+    color: '橘色',
+    size: 'S',
+    price: 2000,
+    productStatus: '上架中',
     address: 'No. 189, Grove St, Los Angeles',
-    zip: 'CA 90036',
+    tag: 'Office',
+  },
+  {
+    category: '衣服',
+    productName: '超炫衣服',
+    color: '橘色',
+    size: 'S',
+    price: 2000,
+    productStatus: '上架中',
+    address: 'No. 189, Grove St, Los Angeles',
+    tag: 'Office',
+  },
+  {
+    category: '衣服',
+    productName: '超炫衣服',
+    color: '橘色',
+    size: 'S',
+    price: 2000,
+    productStatus: '上架中',
+    address: 'No. 189, Grove St, Los Angeles',
+    tag: 'Office',
+  },
+  {
+    category: '衣服',
+    productName: '超炫衣服',
+    color: '橘色',
+    size: 'S',
+    price: 2000,
+    productStatus: '上架中',
+    address: 'No. 189, Grove St, Los Angeles',
+    tag: 'Office',
+  },
+  {
+    category: '衣服',
+    productName: '超炫衣服',
+    color: '橘色',
+    size: 'S',
+    price: 2000,
+    productStatus: '上架中',
+    address: 'No. 189, Grove St, Los Angeles',
+    tag: 'Office',
+  },
+  {
+    category: '衣服',
+    productName: '超炫衣服',
+    color: '橘色',
+    size: 'S',
+    price: 2000,
+    productStatus: '上架中',
+    address: 'No. 189, Grove St, Los Angeles',
+    tag: 'Office',
+  },
+  {
+    category: '衣服',
+    productName: '超炫衣服',
+    color: '橘色',
+    size: 'S',
+    price: 2000,
+    productStatus: '上架中',
+    address: 'No. 189, Grove St, Los Angeles',
     tag: 'Office',
   },
 ];
