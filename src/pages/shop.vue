5fa1bcec56c04c8410bfb3625ebe45cde118f296
kuankuan0726 <kuankuan0726@gmail.com>
2023-05-16 13:16:38 +0800

    更新

--- src/pages/shop.vue
+++ src/pages/shop.vue
@@ -1,5 +1,4 @@
 <template>
-  <div class="mainbox">
   <Bar />
 
       <main>
@@ -10,16 +9,11 @@
           <Customization />
           <create />
       </main>
-  </div>
 </template>
 <script setup>
 
 </script>
 <style lang="scss" scoped>
-
-.mainbox {
-  display: flex;
-}
 
 main {
   padding-top: 0;
