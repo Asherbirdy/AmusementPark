91493041ee0af5df151f6553b10463ef3150a48a
Asherbirdy <122598799+Asherbirdy@users.noreply.github.com>
2023-05-18 19:54:03 +0800

    新增json檔案給竹安 (#100)

---
+++ src/pages/CRUD_RUD.vue
@@ -0,0 +1,17 @@
+<script setup>
+import axios from 'axios';
+
+const username = ref('asher');
+
+axios.get('../../src/assets/json/ticketInfo.json').then(res => {
+  console.log(res.data);
+});
+</script>
+
+<template>
+  <div></div>
+</template>
+
+<style scoped lang="scss">
+//
+</style>
