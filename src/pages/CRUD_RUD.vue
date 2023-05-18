8f8b3fdd87a08ab813d03de42669a1d528a7748d
Asherbirdy <birdym.tw@gmail.com>
2023-05-18 20:37:15 +0800

    建立檔案

--- src/pages/CRUD_RUD.vue
+++ src/pages/CRUD_RUD.vue
@@ -2,14 +2,25 @@
 import axios from 'axios';
 
 const username = ref('asher');
+let bookingData = reactive({});
 
-axios.get('../../src/assets/json/ticketInfo.json').then(res => {
-  console.log(res.data);
+onMounted(() => {
+  axios.get('../../src/assets/json/ticketInfo.json').then(res => {
+    bookingData.value = res.data.ticketBooking;
+    console.log(bookingData);
+  });
 });
 </script>
 
 <template>
-  <div></div>
+  <!-- <h1>sd</h1> -->
+  <!-- 顯示畫面 -->
+  <div v-for="(item, i) in bookingData.value">
+    <div style="display: flex">
+      <p style="margin-bottom: 10px">{{ item }}</p>
+      <button style="margin-bottom: 10px">退票</button>
+    </div>
+  </div>
 </template>
 
 <style scoped lang="scss">
