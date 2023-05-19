d3149d39be26687a17ced20ab67c20d70f75d124
kuankuan0726 <kuankuan0726@gmail.com>
2023-05-19 15:32:52 +0800

    更新Bar

--- src/components/product/Bar.vue
+++ src/components/product/Bar.vue
@@ -1,26 +1,38 @@
 <template>
-<div class="navbar">
-            <ul>
-                <li class="bar">所有商品</li>
-                <br>
-                <li class="bar">衣服</li>
-                <br>
-                <li class="bar">帽子</li>
-                <br>
-                <li class="bar">帆布袋</li>
-                <br>
-                <li class="bar">手機殼</li>
-                <br>
-                <li class="bar">客製化商品</li>
-                <br>
-            </ul>
-        </div>
+    <div class="sidebar">
+        <ul>
+            <li class="bar">所有商品</li>
+            <br>
+            <li class="bar">衣服</li>
+            <br>
+            <li class="bar">帽子</li>
+            <br>
+            <li class="bar">帆布袋</li>
+            <br>
+            <li class="bar">手機殼</li>
+            <br>
+            <li class="bar">客製化商品</li>
+            <br>
+        </ul>
+    </div>
 </template>
 <script setup>
+document.addEventListener('scroll', function () {
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    console.log(scrollTop);
+
+    var sidebar = document.querySelector('.sidebar');
+    if (scrollTop > 150) {
+        sidebar.style.top = '0';
+    } else {
+        sidebar.style.top = '';
+    }
+});
+
 let bar = { bar: 'bar' };
 </script>
 <style lang="scss" scoped>
-.navbar {
+.sidebar {
     width: 180px;
     display: inline-block;
     padding: 30px;
@@ -28,6 +40,7 @@ let bar = { bar: 'bar' };
     top: 125px;
     left: 0;
 }
+
 .bar {
     color: $textcolor7;
     padding: 10px;
