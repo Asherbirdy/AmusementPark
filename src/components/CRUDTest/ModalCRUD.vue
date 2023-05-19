9a78abd851b4b7ebe2f0eb3cc834c7461464166f
Asherbirdy <birdym.tw@gmail.com>
2023-05-19 08:30:55 +0800

    點擊彈窗事件

---
+++ src/components/CRUDTest/ModalCRUD.vue
@@ -0,0 +1,22 @@
+<template>
+  <el-dialog v-model="isOpen" title="退票資訊" width="30%" center>
+    <span>
+      It should be noted that the content will not be aligned in center by
+      default
+    </span>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="isOpen = false">Cancel</el-button>
+        <el-button type="primary" @click="isOpen = false"> Confirm </el-button>
+      </span>
+    </template>
+  </el-dialog>
+</template>
+<script setup>
+const isOpen = ref(true);
+</script>
+<style scoped>
+.dialog-footer button:first-child {
+  margin-right: 10px;
+}
+</style>
--- src/pages/CRUD_RUD.vue
+++ src/pages/CRUD_RUD.vue
@@ -3,24 +3,33 @@ import axios from 'axios';
 
 const username = ref('asher');
 let bookingData = reactive({});
-
+// 先預設彈窗是隱藏
+let showmodal = ref(false);
 onMounted(() => {
   axios.get('../../src/assets/json/ticketInfo.json').then(res => {
     bookingData.value = res.data.ticketBooking;
     console.log(bookingData);
   });
 });
+
+// 當執行 openModal 會把 showmodal 變成 true
+const openModal = () => {
+  showmodal.value = true;
+};
 </script>
 
 <template>
-  <!-- <h1>sd</h1> -->
   <!-- 顯示畫面 -->
   <div v-for="(item, i) in bookingData.value">
     <div style="display: flex">
-      <p style="margin-bottom: 10px">{{ item }}</p>
-      <button style="margin-bottom: 10px">退票</button>
+      <p style="margin-bottom: 10px" :id="'btn' + i">{{ item }}</p>
+      <button style="margin-bottom: 10px" :id="'ticket' + i" @click="openModal">
+        退票
+      </button>
     </div>
   </div>
+  <!--  -->
+  <modal-c-r-u-d v-model="showmodal"></modal-c-r-u-d>
 </template>
 
 <style scoped lang="scss">
