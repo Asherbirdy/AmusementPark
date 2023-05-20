e77e72303874293b23b6854de636b2a5cf068e97
Asherbirdy <122598799+Asherbirdy@users.noreply.github.com>
2023-05-20 19:22:01 +0800

    遊客後臺 訂票 (彈窗範例)
    * 使用V-FOR
    
    * 整理資料結構
    
    * 建立展演JSON測試資料
    
    * 更新
    
    * hover消失
    
    * 修改hover bug
    
    * 彈窗顯示
    
    * 更新
    
    * 彈窗 淡近淡出
    
    * 更新文字

--- vite.config.js
+++ vite.config.js
@@ -14,11 +14,6 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/AmusementPark/',
-  build: {
-    rollupOptions: {
-      external: ['gsap'],
-    },
-  },
   plugins: [
     vue(),
     AutoImport({
