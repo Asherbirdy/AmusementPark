8afc341f736276202ff4ff831b71133a74d61f51
Asherbirdy <birdym.tw@gmail.com>
2023-05-21 13:50:17 +0800

    商品管理-訂單系統UI

--- src/components/Productstatus/TicketListPDS.vue
+++ src/components/Productstatus/TicketListPDS.vue
@@ -5,24 +5,28 @@ const tableData = [
     orderDate: '2016-05-02',
     numbers: 3,
     ticketType: '成人票',
+    fastForward: ['設施一', '設施五', '設施六'],
   },
   {
     id: 2,
     orderDate: '2016-05-04',
     numbers: 4,
     ticketType: '兒童票',
+    fastForward: ['設施一', '設施三'],
   },
   {
     id: 3,
     orderDate: '2016-05-01',
     numbers: 1,
     ticketType: '優惠票',
+    fastForward: ['設施一', '設施三', '設施四', '設施六'],
   },
   {
     id: 4,
     orderDate: '2016-05-03',
     numbers: 5,
     ticketType: '成人票',
+    fastForward: ['設施一'],
   },
 ];
 </script>
@@ -37,6 +41,7 @@ const tableData = [
     <el-table-column prop="orderDate" label="訂票時間" sortable />
     <el-table-column prop="numbers" label="數量" />
     <el-table-column prop="ticketType" label="票種" />
+    <el-table-column prop="fastForward" label="快速通關設施" />
   </el-table>
 </template>
 <style scoped lang="scss">
