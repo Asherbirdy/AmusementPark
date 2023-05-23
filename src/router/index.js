07cf38659945b6e4216b9dcd50b13a4c6fb1e099
rusheep <yo0983987618@gmail.com>
2023-05-23 14:38:37 +0800

    createWebHashHistory

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import { setupLayouts } from "virtual:generated-layouts";
 import generatedRoutes from "virtual:generated-pages";
 
@@ -8,7 +8,7 @@ generatedRoutes.forEach((v) => {
 });
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
 });
 
