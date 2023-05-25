1d9089fa69c33c2247b28cdf8179046a49d148a3
Asherbirdy <122598799+Asherbirdy@users.noreply.github.com>
2023-05-25 13:20:30 +0800

    遊客前台 訂票頁面(放入購物車)
    * 購票Form
    
    * 修改
    
    * 完成日期
    
    * 完成加入購物車
    
    * 完成

--- src/components/TicketOrder/TicketOrder.vue
+++ src/components/TicketOrder/TicketOrder.vue
@@ -1,93 +1,239 @@
-<script setup></script>
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+// 資料
+let bookingData = reactive([
+  {
+    adult: {
+      ticketNum: 0,
+      fastForwad: false,
+    },
+  },
+  {
+    student: {
+      ticketNum: 0,
+      fastForwad: false,
+    },
+  },
+  {
+    children: {
+      ticketNum: 0,
+      fastForwad: false,
+    },
+  },
+  {
+    concession: {
+      ticketNum: 1,
+      fastForwad: false,
+    },
+  },
+]);
+
+function countTicket() {
+  return bookingData.reduce((accumulator, current) => {
+    const ticketNum = Object.values(current)[0].ticketNum;
+    return accumulator + ticketNum;
+  }, 0);
+}
+
+
+// 清空按鈕
+const clearOut = () => {
+  bookingData[0].adult.ticketNum = 0;
+  bookingData[0].adult.fastForwad = false;
+  bookingData[1].student.ticketNum = 0;
+  bookingData[1].student.fastForwad = false;
+  bookingData[2].children.ticketNum = 0;
+  bookingData[2].children.fastForwad = false;
+  bookingData[3].concession.ticketNum = 0;
+  bookingData[3].concession.fastForwad = false;
+  // 清空localStorage
+  localStorage?.removeItem('bookingData');
+  localStorage?.removeItem('ticketDateData');
+};
+
+// 將資料訪到local函式：
+const addBookingDataToLocal = item =>
+  localStorage.setItem('bookingData', JSON.stringify({ item }));
+const addTicketDateToLocal = item =>
+  localStorage.setItem('ticketDateData', JSON.stringify({ item }));
+
+let ticketDate = ref('');
+
+const handleDateSelected = date => {
+  ticketDate = date;
+  return date;
+};
+
+const isValidDateFormat = dateString => {
+  const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
+  return dateRegex.test(dateString);
+};
+
+// 夾到購物車
+const addToCart = () => {
+  if (ticketDate !== '' && isValidDateFormat(ticketDate)) {
+    if (countTicket() !== 0) {
+      console.log(ticketDate, countTicket(), 'ticketDate 執行加入購物車');
+      addBookingDataToLocal(bookingData);
+      addTicketDateToLocal(ticketDate);
+      alert('已將票數加入到購物車');
+    } else {
+      alert('請加入票數');
+    }
+  } else {
+    alert('請輸入日期');
+  }
+};
+
+const buyTicket = () => {
+  addBookingDataToLocal(bookingData);
+  addTicketDateToLocal(ticketDate);
+  router.push('/cart');
+};
+
+// 收集父層邏輯
+</script>
 <template>
+  <!-- 日曆： -->
+  <!-- <ticket-use-time id="orderdate" @selectDate="handleSelectDate"/> -->
+
   <div>
+    <div class="chooseDate">
+      <ChooseDateTIC @date-selected="handleDateSelected" />
+    </div>
+
     <table>
       <tr>
         <th>票種</th>
-        <th>適用對象</th>  
+        <th>適用對象</th>
         <th>票價</th>
         <th>張數</th>
         <th>快速通關</th>
       </tr>
       <tr>
-        <td>全票</td>        
+        <td>全票</td>
         <td class="ex">一般成人，身高110cm以上視為成人</td>
         <td>NT.500</td>
         <td>
-          <el-input-number 
-            v-model="num" width:10px
+          <el-input-number
+            v-model="bookingData[0].adult.ticketNum"
             :min="0"
-            @change="handleChange" class="count"
-          /></td>
-          <td><input type="checkbox"></td>
+            class="count"
+            width:10px
+          />
+        </td>
+        <td>
+          <input v-model="bookingData[0].adult.fastForwad" type="checkbox" />
+        </td>
       </tr>
       <tr>
-        <td>學生票</td>        
+        <td>學生票</td>
         <td class="ex">入園需持當學期註冊學生證之學生(限本人使用)</td>
         <td>NT.400</td>
         <td>
-          <el-input-number 
-            v-model="num" width:10px
+          <el-input-number
+            v-model="bookingData[1].student.ticketNum"
             :min="0"
-            @change="handleChange" class="count"
-          /></td>
-          <td><input type="checkbox"></td>
+            class="count"
+            width:10px
+          />
+        </td>
+        <td>
+          <input v-model="bookingData[1].student.fastForwad" type="checkbox" />
+        </td>
       </tr>
       <tr>
-        <td>兒童票</td>       
+        <td>兒童票</td>
         <td class="ex">4歲~12歲，身高未滿110cm兒童</td>
         <td>NT.250</td>
         <td>
-          <el-input-number 
-            v-model="num" width:10px
+          <el-input-number
+            v-model="bookingData[2].children.ticketNum"
             :min="0"
-            @change="handleChange" class="count"
-          /></td>
-          <td><input type="checkbox"></td>
+            class="count"
+            width:10px
+          />
+        </td>
+        <td>
+          <input v-model="bookingData[2].children.fastForwad" type="checkbox" />
+        </td>
       </tr>
       <tr>
-        <td>優待票</td>        
+        <td>優待票</td>
         <td class="ex">持有身心障礙證明者與1位陪同者、孕婦、65歲以上長者</td>
         <td>NT.200</td>
         <td>
-          <el-input-number 
-            v-model="num" width:10px
+          <el-input-number
+            v-model="bookingData[3].concession.ticketNum"
             :min="0"
-            @change="handleChange" class="count"
-          /></td>
-          <td><input type="checkbox"></td>
+            class="count"
+            width:10px
+          />
+        </td>
+        <td>
+          <input
+            v-model="bookingData[3].concession.fastForwad"
+            type="checkbox"
+          />
+        </td>
       </tr>
-      <tr><td colspan="10" class="ex">4歲以下免費入場</td></tr>
+      <tr>
+        <td colspan="10" class="ex">4歲以下免費入場</td>
+      </tr>
     </table>
-    
+  </div>
+  <div class="btnbox">
+    <btn
+      class="btn"
+      button-color="#D1825B"
+      button-text-color="white"
+      @click="clearOut"
+    >
+      <h3>清空</h3>
+    </btn>
+    <btn
+      :style="{ width: '150px' }"
+      class="btn"
+      button-color="#D1825B"
+      button-text-color="white"
+      @click="addToCart"
+    >
+      <h3>加入購物車</h3>
+    </btn>
+    <btn
+      class="btn"
+      button-color="#D1825B"
+      button-text-color="white"
+      @click="buyTicket"
+    >
+      <h3>立即購買</h3>
+    </btn>
   </div>
 </template>
 
-<!-- <script>
-export default {
-  name: 'MyComponent',
-  data() {
-    return {
-      num: 0,
-    }
-  },
-  methods: {
-    handleChange(value) {
-      console.log(value)
-    },
-  },
-}
-</script> -->
 <style lang="scss" scoped>
-table,th,td {
-  border: 2px solid #d1825b;
-  
+.chooseDate {
+  display: flex;
+  align-items: center;
+  gap: 30px;
+  margin-left: 60px;
+  margin-bottom: 20px;
 }
 
-th,td {
+table,
+th,
+td {
+  border: 2px solid #d1825b;
+}
+
+th,
+td {
   padding: 8px;
 }
-table{
+
+table {
   margin: 0 auto;
   // width: 1000px;
 
@@ -104,10 +250,20 @@ table{
       text-align-last: center;
       font-size: 24px;
     }
-    .ex{
+
+    .ex {
       text-align-last: left;
     }
   }
 }
 
+.btnbox {
+  width: 400px;
+  display: flex;
+  float: right;
+  justify-content: space-between;
+  margin-bottom: 100px;
+  margin-right: 60px;
+  margin-top: 20px;
+}
 </style>
