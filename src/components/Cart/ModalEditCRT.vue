a3457e72aabc7956f948174dff81e40181bdef10
Asher Lien <birdym.tw@gmail.com>
2023-06-06 16:28:28 +0800

    完成未登入 購物車修改

--- src/components/Cart/ModalEditCRT.vue
+++ src/components/Cart/ModalEditCRT.vue
@@ -24,9 +24,9 @@
 <script setup>
 import axios from 'axios';
 import { nextTick, onMounted } from 'vue';
-import { useTest } from '../../composables';
+import { useTest, getSessionBookingData } from '../../composables';
 // 外部資料導入資訊
-const emit = defineEmits(['show-order']);
+const emit = defineEmits(['show-order', 'update-session']);
 
 const props = defineProps({
   ticketType: {
@@ -43,14 +43,12 @@ const props = defineProps({
   },
   ticketId: {
     type: Number,
-    required: true,
   },
   orderId: {
     type: Number,
-    required: true,
   },
   fassPass: {
-    type: Number,
+    type: Boolean,
     required: true,
   },
 });
@@ -67,39 +65,64 @@ const handleChange = value => {
   ticketNum.value = value;
 };
 
+console.log('傳值', props);
+
 // 退票按鈕函式
 const fixTickets = () => {
-  console.log('傳值', props);
   console.log('修改票數', ticketNum.value, '張');
-  const ticketType = props.ticketType.split(' ')[1];
-  console.log('票型', ticketType);
+  if (sessionStorage.getItem('token')) {
+    const transferToDB = reactive({
+      ORDER_ID: props.orderId,
+      TICK_ORDER_ID: props.ticketId,
+      TICK_NUM: ticketNum.value,
+      TOTAL_PRICE: props.ticketPrice * ticketNum.value,
+    });
 
-  const transferToDB = reactive({
-    ORDER_ID: props.orderId,
-    TICK_ORDER_ID: props.ticketId,
-    TICK_NUM: ticketNum.value,
-    TOTAL_PRICE: props.ticketPrice * ticketNum.value,
-  });
-
-  // edit函式
-  async function editTick() {
-    try {
-      const response = await axios.post(
-        '/PDO/frontEnd/cart/cartUpdate.php',
-        transferToDB
-      );
-      // 在此處處理回傳的 response
-      emit('show-order');
-      console.log(response);
-    } catch (error) {
-      // 在此處處理錯誤
-      console.error(error);
+    // edit函式
+    async function editTick() {
+      const ticketType = props.ticketType.split(' ')[1];
+      console.log('票型', ticketType);
+      try {
+        const response = await axios.post(
+          '/PDO/frontEnd/cart/cartUpdate.php',
+          transferToDB
+        );
+        // 在此處處理回傳的 response
+        emit('show-order');
+        console.log(response);
+        console.log(transferToDB);
+      } catch (error) {
+        // 在此處處理錯誤
+        console.error(error);
+      }
     }
+    editTick();
+  } else {
+    console.log('未登入');
+    // 要在session找到的值：
+    const editData = {
+      ticketType: props.ticketType.split(' ')[1],
+      ticketData: props.ticketType.split(' ')[0],
+      fastFoward: props.fassPass,
+    };
+
+    const sessionUnlogin = getSessionBookingData();
+    console.log('session', sessionUnlogin);
+    const editTicket = sessionUnlogin.find(
+      ticket =>
+        ticket.ticketType === editData.ticketType &&
+        ticket.ticketData === editData.ticketData &&
+        ticket.fastFoward === editData.fastFoward
+    );
+    if (editTicket) {
+      editTicket.tickets = ticketNum.value;
+    }
+    console.log('updated session', sessionUnlogin);
+    // 更新sessionStorage:
+    sessionStorage.setItem('bookingData', JSON.stringify(sessionUnlogin));
+    emit('update-session', sessionUnlogin);
   }
 
-  editTick();
-
-  console.log(transferToDB);
   ticketNum.value = 1;
 };
 </script>
